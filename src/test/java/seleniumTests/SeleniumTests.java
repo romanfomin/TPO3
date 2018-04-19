c84d412a501413f57ebba7b70ec241ce2baf48dd
romanfomin <rofominucvt@mail.ru>
2018-04-19 20:30:51 +0300

    all tests

--- src/test/java/seleniumTests/SeleniumTests.java
+++ src/test/java/seleniumTests/SeleniumTests.java
@@ -1,17 +1,26 @@
 package seleniumTests;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import utils.SeleniumUtils;
 
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SeleniumTests {
 
-    SeleniumUtils utils = new SeleniumUtils();
+    private SeleniumUtils utils = new SeleniumUtils();
+
+    @AfterEach
+    private void testEnd() {
+        for (WebDriver driver : utils.getWebDrivers()) {
+            driver.quit();
+        }
+    }
 
     @Test
     public void testChangeLanguage() {
@@ -50,8 +59,109 @@ public class SeleniumTests {
             Set<String> windowHandles = driver.getWindowHandles();
             for (String handle : windowHandles) {
                 driver.switchTo().window(handle);
-                if (driver.getTitle().contains("win_ser_1")) {
+                if (driver.getTitle().contains("Acesse_Terms_Of_Use")) {
                     assertEquals("WEBSITE TERMS OF USE", driver.findElement(By.xpath("//div[@class=\"textLayer\"]/div[2]")).getText());
+                    break;
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testLink1() {
+        for (WebDriver driver : utils.getWebDrivers()) {
+            driver.get("https://acesse.com/en");
+            driver.findElement(By.xpath("//a[contains(@href,\"gamesmart\")]")).click();
+            Set<String> windowHandles = driver.getWindowHandles();
+            for (String handle : windowHandles) {
+                driver.switchTo().window(handle);
+                if (driver.getTitle().contains("win_ser_1")) {
+                    assertEquals("GameSmart", driver.getTitle());
+                    break;
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testLink2() {
+        for (WebDriver driver : utils.getWebDrivers()) {
+            driver.get("https://acesse.com/en");
+            driver.findElement(By.xpath("//a[contains(@href,\"asports\")]")).click();
+            Set<String> windowHandles = driver.getWindowHandles();
+            for (String handle : windowHandles) {
+                driver.switchTo().window(handle);
+                if (driver.getTitle().contains("win_ser_1")) {
+                    assertEquals("ACESSE ESPORTS - Start playing fantasy eSports today!", driver.getTitle());
+                    break;
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testRegisterPage() {
+        for (WebDriver driver : utils.getWebDrivers()) {
+            driver.get("https://acesse.com/en");
+            driver.findElement(By.xpath("//a[contains(@href,\"signup\")]")).click();
+            WebElement element = driver.findElement(By.xpath("//input[@type=\"submit\"]"));
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);",element);
+            element.click();
+            assertTrue(driver.findElement(By.xpath("//div[contains(@class,\"alert alert-danger\")]/b")).getText().matches("^Warning You have entered an Invalid Sponsor ID[\\s\\S]*$"));
+        }
+    }
+
+    @Test
+    public void testLogInWithNoPassword() {
+        for (WebDriver driver : utils.getWebDrivers()) {
+            driver.get("https://acesse.com/en");
+            driver.findElement(By.xpath("//a[contains(@href,\"login\")]")).click();
+            driver.findElement(By.xpath("//input[@name=\"Login\"]")).click();
+            driver.findElement(By.xpath("//input[@name=\"Login\"]")).clear();
+            driver.findElement(By.xpath("//input[@name=\"Login\"]")).sendKeys("123");
+            driver.findElement(By.xpath("//input[@name=\"turing\"]")).click();
+            driver.findElement(By.xpath("//input[@name=\"turing\"]")).clear();
+            driver.findElement(By.xpath("//input[@name=\"turing\"]")).sendKeys("125");
+            WebElement element = driver.findElement(By.xpath("//input[@value='Log in']"));
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);",element);
+            element.click();
+            assertEquals("You should specify this field", driver.findElement(By.xpath("//label[contains(text(),\"Password\")]//following::span[contains(@style,\"color: red\")]")).getText());
+            assertEquals("Incorrect digits", driver.findElement(By.xpath("//body[@id='app-layout']/div/div/form/div[4]/span")).getText());
+        }
+    }
+
+    @Test
+    public void testLogInWithWrongTuring() {
+        for (WebDriver driver : utils.getWebDrivers()) {
+            driver.get("https://acesse.com/en");
+            driver.findElement(By.xpath("//a[contains(@href,\"login\")]")).click();
+            driver.findElement(By.xpath("//input[@name=\"Login\"]")).click();
+            driver.findElement(By.xpath("//input[@name=\"Login\"]")).clear();
+            driver.findElement(By.xpath("//input[@name=\"Login\"]")).sendKeys("123");
+            driver.findElement(By.xpath("//input[@name=\"Password\"]")).click();
+            driver.findElement(By.xpath("//input[@name=\"Password\"]")).clear();
+            driver.findElement(By.xpath("//input[@name=\"Password\"]")).sendKeys("125");
+            driver.findElement(By.xpath("//input[@name=\"turing\"]")).click();
+            driver.findElement(By.xpath("//input[@name=\"turing\"]")).clear();
+            driver.findElement(By.xpath("//input[@name=\"turing\"]")).sendKeys("125");
+            driver.findElement(By.xpath("//input[@value='Log in']")).click();
+            assertEquals("Incorrect digits", driver.findElement(By.xpath("//label[contains(text(),\"Turing\")]//following::span[contains(@style,\"color: red\")]")).getText());
+        }
+    }
+
+    @Test
+    public void testFacebookLink() {
+        for (WebDriver driver : utils.getWebDrivers()) {
+            driver.get("https://acesse.com/en");
+            WebElement element = driver.findElement(By.xpath("//div[contains(@class,\"soc-media\")]/ul/li[1]/a"));
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);",element);
+            element.click();
+            Set<String> windowHandles = driver.getWindowHandles();
+            for (String handle : windowHandles) {
+                driver.switchTo().window(handle);
+                if (driver.getTitle().contains("win_ser_1")) {
+                    assertEquals("Facebook", driver.findElement(By.xpath("//i[contains(@class,\"fb_logo\")]/u")).getText());
+                    break;
                 }
             }
         }
--- src/test/java/utils/SeleniumUtils.java
+++ src/test/java/utils/SeleniumUtils.java
@@ -12,6 +12,24 @@ public class SeleniumUtils {
 
     private WebDriver firefoxDriver;
 
+    private WebDriver chromeDriver;
+    private String baseUrl = "http://acesse.com/en";
+
+    private List<WebDriver> webDrivers = new ArrayList<>();
+
+    public SeleniumUtils(){
+        System.setProperty("webdriver.gecko.driver","/home/fomin/drivers/geckodriver-v0.20.1-linux64/geckodriver");
+        System.setProperty("webdriver.chrome.driver","/home/fomin/drivers/chromedriver_linux64/chromedriver");
+        chromeDriver = new ChromeDriver();
+        firefoxDriver=new FirefoxDriver();
+        webDrivers.add(firefoxDriver);
+        webDrivers.add(chromeDriver);
+        for (WebDriver webDriver : webDrivers) {
+            webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+            webDriver.get(baseUrl);
+        }
+    }
+
     public WebDriver getFirefoxDriver() {
         return firefoxDriver;
     }
@@ -42,23 +60,5 @@ public class SeleniumUtils {
 
     public void setWebDrivers(List<WebDriver> webDrivers) {
         this.webDrivers = webDrivers;
-    }
-
-    private WebDriver chromeDriver;
-    private String baseUrl = "http://acesse.com/en";
-
-    private List<WebDriver> webDrivers = new ArrayList<WebDriver>();
-
-    public SeleniumUtils(){
-        System.setProperty("webdriver.gecko.driver","/home/fomin/drivers/geckodriver-v0.20.1-linux64/geckodriver");
-        System.setProperty("webdriver.chrome.driver","/home/fomin/drivers/chromedriver_linux64/chromedriver");
-        chromeDriver = new ChromeDriver();
-        firefoxDriver=new FirefoxDriver();
-        webDrivers.add(firefoxDriver);
-        webDrivers.add(chromeDriver);
-        for (WebDriver webDriver : webDrivers) {
-            webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-            webDriver.get(baseUrl);
-        }
     }
 }
